799c7ccda28eb995675e75626727f81d636ee734
OnurGuven35 <gvnn.onrr@gmail.com>
2022-06-03 16:41:11 +0300

    Initial commit

---
+++ src/Power.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int n;
+        System.out.print("Sinir Degerini Giriniz : ");
+        n = inp.nextInt();
+
+        if (n < 0) {
+            System.out.print("Lutfen Tam Sayi Giriniz ! ");
+        } else {
+            System.out.print("4'un Usleri : ");
+            for (int i = 1; i <= n; i *= 4) {
+                System.out.print(i + ",");
+            }
+
+            System.out.println("");
+            System.out.print("5'in Usleri : ");
+
+            for (int i = 1; i <= n; i *= 5) {
+                System.out.print(i + ",");
+            }
+        }
+    }
+}
